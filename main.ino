7af3138644d1cfe262279d402f8aa4a4f402426a
Xnork <xnorkdeuzivan@gmail.com>
2023-10-12 01:04:07 -0300

    (add): Page new and free_page

--- main.ino
+++ main.ino
@@ -1,6 +1,5 @@
 #define EMULATE 1
 
-
 #if EMULATE
 	#include <stdlib.h>
 	#include <stdio.h>
@@ -11,12 +10,12 @@
 	#include <Arduino.h>
 #endif
 
+// --> Option {
 typedef struct _Option
 {
 	char *label;
 	bool is_active;
 } Option;
-
 Option *new_option(const char *label)
 {
 	Option *option = (Option*) malloc(sizeof(Option));
@@ -37,17 +36,48 @@ void dump_option(Option *option)
 	#if EMULATE
 		printf("Option: %p\nLabel: %s\nIs_Active: %d\n", option, option->label, option->is_active);
 	#else 
-
+		Serial.print("Option: %p\nLabel: %s\nIs_Active: %d\n", option, option->label, option->is_active);
 	#endif
 }
+// } Option;
+
+// --> Page {
+typedef struct _Page
+{
+	Option **options;
+	int amount_of_option;
+} Page;
+
+Page *new_page(int amount_of_option)
+{
+	Page *page = (Page*) malloc(sizeof(Page));
+
+	page->options = (Option**) malloc(sizeof(Option*) * amount_of_option);
+	page->amount_of_option = amount_of_option;
+
+	return page;
+}
+void free_page(Page *page)
+{
+	for(int o = 0; o < page->amount_of_option; o++)
+		if(page->options[o] != NULL)
+			free_option(page->options[o]);
+	free(page);
+}
+// } Page;
 
 void setup()
 {
-	Option *option = new_option("Scan Wi-Fi");
+	Page *page = new_page(2);
 
-	dump_option(option);
+	page->options[0] = new_option("Scan Wi-Fi");
+	page->options[1] = new_option("Clone RFID");
 
-	free_option(option);
+	free_page(page);
+	
+	#if !EMULATE
+		Serial.begin(9600);
+	#endif
 }
 void loop()
 {
